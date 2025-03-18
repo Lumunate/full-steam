9206b2927cabd4147e56d216a7f6de040611ae4d
Faizan Haq <fizoneechan@gmail.com>
2025-03-19 03:29:47 +0500

    fix layouts and rendering

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,29 +1,27 @@
-import '@/styles/globals.css';
-
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 
-import { fetchMessages } from '@/lib/i18n/getMessage';
-import theme from '@/lib/theme';
+import { routing } from '@/i18n/routing';
 
-export default async function RootLayout({ children, params }: { children: React.ReactNode; params: { locale: string } }) {
-  const queryClient = new QueryClient();
-  const messages = await fetchMessages(params.locale);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ParamsWithLocale = Promise<{ locale: any }>;
+
+export default async function RootLayout({
+  children, params,
+}: Readonly<{ children: React.ReactNode, params: ParamsWithLocale }>) {
+
+  const { locale } = await params;
+
+  if (!routing.locales.includes(locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages({ locale });
 
   return (
-    <html lang={params.locale}>
-      <body>
-        <NextIntlClientProvider messages={messages} locale={params.locale}>
-          <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              {children}
-            </ThemeProvider>
-          </QueryClientProvider>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider messages={messages}>
+      {children}
+    </NextIntlClientProvider>
   );
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+import '@/styles/globals.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import React from 'react';
+
+import { SnackbarProvider } from '@/components/snackbar';
+import { ThemeRegistry } from '@/lib/theme';
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const queryClient = new QueryClient();
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <SnackbarProvider>
+            <ThemeRegistry>
+              <NuqsAdapter>{children}</NuqsAdapter>
+            </ThemeRegistry>
+          </SnackbarProvider>
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
+}
