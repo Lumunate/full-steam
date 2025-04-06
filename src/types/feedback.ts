756e708fe4b783e7e163ab7c4117b8bc1a4811b4
Haris Ahmad <haris341998@gmail.com>
2025-04-06 22:30:53 +0500

    Deleted unnecessary files

--- src/types/feedback.ts
+++ src/types/feedback.ts
@@ -3,7 +3,9 @@ import { z } from 'zod';
 export const feedbackSchema = z.object({
   name: z.string().min(1, 'Name is required'),
   lastName: z.string().min(1, 'Last Name is required'),
-  course: z.string().min(1, 'Course/Subject is required'),
+  course: z.string().min(1, 'Course/Subject is required').optional(),
+  relation: z.string().min(1, 'Relation is required'),
+  email: z.string().email('Invalid email').optional(),
   sessionDate: z.preprocess((val) => {
     if (typeof val === 'string') {
       const date = new Date(val);
